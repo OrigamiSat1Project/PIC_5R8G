2f5d35343bf95c331b199b8167bc1ec18527975b
ryogaide <ide.r.aa@m.titech.ac.jp>
2018-05-27 16:03:44 +0900

    Upadate : Erase_sectors_before_Write in order to adapt Change_adr

--- PIC_5R8G.X/source/SECTOR.c
+++ PIC_5R8G.X/source/SECTOR.c
@@ -11,8 +11,8 @@ void Erase_sectors(UBYTE Sector_start_byte, UBYTE Amount_of_erase_sector){
     * Receive sector's identificate address and how many sectors want to delete, delete sectors from received sector.
     * ======================================================================================
     */
-    UDWORD FROM_sector_adr = (UDWORD)Sector_start_byte<<16;       //We have to shift 16bit to move sector_start_address
-    for (UBYTE i=0x00; i<Amount_of_erase_sector; i++){      
+    UDWORD FROM_sector_adr = (UDWORD)(Sector_start_byte << 16);       //We have to shift 16bit to move sector_start_address
+    for (UBYTE i=0x00; i<Amount_of_erase_sector; i++){
     flash_Erase(FROM_sector_adr, S_ERASE);
     FROM_sector_adr += 0x10000;                //Jump to next sector which you want to delete
     CLRWDT();
@@ -20,14 +20,15 @@ void Erase_sectors(UBYTE Sector_start_byte, UBYTE Amount_of_erase_sector){
     }
 }
 
-void Erase_sectors_before_Write(UDWORD tmp_adr_erase){
+void Erase_sectors_before_Write(UDWORD tmp_adr_erase, UDWORD Jump_adr){
    /* Comment
-    * ===================================================================================================
+    * ==========================================================================
     * Erase sectors before writing FROM
-    * Original JPEG use 16 sectors and 1/4 JPEG use 8 sectors.
-    * We erase 16 sectors from Roop_adr in this Code.
+    * JPEG will receive by 8split. Each group needs amount of Jump_adr sectors.
+    * So we have to erase Jump_adr*8 sectors
+    * ==========================================================================
     */
-    const UINT Amount_of_erase_sector = 16;
+    UINT Amount_of_erase_sector = (UINT)(Jump_adr >> 16) * 8;
     for (UINT i=0; i<Amount_of_erase_sector; i++){
     flash_Erase(tmp_adr_erase,S_ERASE);
     tmp_adr_erase += 0x10000;         //Jump to next sector's start address

e96b8272396b68aaaab67992bcd6eb925c72c2d0
ryogaide <ide.r.aa@m.titech.ac.jp>
2018-06-04 23:51:14 +0900

    Add function : Controling by CAM1 in H264 function & add FooterOfH264

--- PIC_5R8G.X/source/ReceiveJPEG.c
+++ PIC_5R8G.X/source/ReceiveJPEG.c
@@ -16,31 +16,30 @@ void Receive_8split_JPEG(UDWORD Roop_adr, UDWORD Jump_adr){
     * We erase 16 sectors from Roop_adr in this Code.
        ===================================================================================================
     */
-    Erase_sectors_before_Write(Roop_adr, Jump_adr);
-    
+    Erase_sectors_before_Write(Roop_adr, Jump_adr);   
     UBYTE Buffer[MaxOfMemory];
     UBYTE receiveEndJpegFlag = 0x00;
     UDWORD FROM_Write_adr = Roop_adr;         //Reset FROM_Write_adr
     
-    /*  How to use receiveEndJpegFlag
- * ===============================================================================================================
- *  Bit7            Bit6            Bit5        Bit4        Bit3        Bit2        Bit1                Bit0
- *  8split_End      8split_cnt2     cnt1        cnt0        ----        ----        Flag_0x1E           Flag_0xFF
- *  
- *  Initialize                 = 0x00  (0b00000000)
- *  Detect JPEG marker 0xFF    = 0x01
- *  End of receive JPEG        = 0x03  (0b00000011)
- *  During writing group 1     = 0x00  (0b00000000)
- *  During writing group 2     = 0x10  (0b00010000)
- *  During writing group 3     = 0x20  (0b00100000)
- *  During writing group 4     = 0x30  (0b00110000)
- *  During writing group 5     = 0x40  (0b01000000)
- *  During writing group 6     = 0x50  (0b01010000)
- *  During writing group 7     = 0x60  (0b01100000)
- *  During writing group 8     = 0x70  (0b01110000)
- *  After Writing              = 0x80  (0b10000000)
- * ===============================================================================================================
- */
+   /*  How to use receiveEndJpegFlag
+    * ===============================================================================================================
+    *  Bit7            Bit6            Bit5        Bit4        Bit3        Bit2        Bit1                Bit0
+    *  8split_End      8split_cnt2     cnt1        cnt0        ----        ----        Flag_0x1E           Flag_0xFF
+    *  
+    *  Initialize                 = 0x00  (0b00000000)
+    *  Detect JPEG marker 0xFF    = 0x01
+    *  End of receive JPEG        = 0x03  (0b00000011)
+    *  During writing group 1     = 0x00  (0b00000000)
+    *  During writing group 2     = 0x10  (0b00010000)
+    *  During writing group 3     = 0x20  (0b00100000)
+    *  During writing group 4     = 0x30  (0b00110000)
+    *  During writing group 5     = 0x40  (0b01000000)
+    *  During writing group 6     = 0x50  (0b01010000)
+    *  During writing group 7     = 0x60  (0b01100000)
+    *  During writing group 8     = 0x70  (0b01110000)
+    *  After Writing              = 0x80  (0b10000000)
+    * ===============================================================================================================
+    */
     UINT index_of_Buffer = 0;
     //FIXME : 
     sendChar(0x88);
@@ -87,7 +86,6 @@ void Receive_thumbnail_JPEG(UDWORD Roop_adr, UDWORD Jump_adr){
        ===================================================================================================
     */
     Erase_sectors_before_Write(Roop_adr, Jump_adr);
-    
     UBYTE Buffer[MaxOfMemory];
     UBYTE receiveEndJpegFlag = 0x00;
     UDWORD FROM_Write_adr = Roop_adr;         //Reset FROM_Write_adr
@@ -146,36 +144,52 @@ void Receive_8split_H264(UDWORD Roop_adr, UDWORD Jump_adr){
        ===================================================================================================
     */
     Erase_sectors_before_Write(Roop_adr, Jump_adr);
+    /* Comment
+     * =========================================================================
+     * wait 5V switch
+     * In H264 function, control receiving by not only Flag but CAM1(5V) switch
+     * CAM1 is bit inversion
+     * =========================================================================
+     * Code
+     * =========================================================================
+     * while(CAM1 == 1);
+     * =========================================================================
+     */
     
     UBYTE Buffer[MaxOfMemory];
     UBYTE receiveEndH264Flag = 0x00;
     UDWORD FROM_Write_adr = Roop_adr;         //Reset FROM_Write_adr
     
     /*  How to use receiveEndH264Flag
- * ===============================================================================================================
- *  Bit7            Bit6            Bit5        Bit4        Bit3                Bit2            Bit1                                Bit0
- *  8split_End      8split_cnt2     cnt1        cnt0        Flag_0x1e        Flag_0x01        Flag_0x00(Second time)           Flag_0x00(First time)
- *  
- *  Initialize                 = 0x00  (0b00000000)
- *  Receive 0x00 (First)       = 0x01  (0b00000001)
- *  Receive 0x00 (Second)      = 0x03  (0b00000011)
- *  Receive 0x01               = 0x07  (0b00000111)
- *  During writing group 1     = 0x00  (0b00000000)
- *  During writing group 2     = 0x10  (0b00010000)
- *  During writing group 3     = 0x20  (0b00100000)
- *  During writing group 4     = 0x30  (0b00110000)
- *  During writing group 5     = 0x40  (0b01000000)
- *  During writing group 6     = 0x50  (0b01010000)
- *  During writing group 7     = 0x60  (0b01100000)
- *  During writing group 8     = 0x70  (0b01110000)
- *  After Writing              = 0x80  (0b10000000)
- * ===============================================================================================================
- */
+     * ===============================================================================================================
+     *  Bit7            Bit6            Bit5        Bit4        Bit3                Bit2            Bit1                                Bit0
+     *  8split_End      8split_cnt2     cnt1        cnt0        Flag_0x1e        Flag_0x01        Flag_0x00(Second time)           Flag_0x00(First time)
+     *  
+     *  Initialize                 = 0x00  (0b00000000)
+     *  Receive 0x00 (First)       = 0x01  (0b00000001)
+     *  Receive 0x00 (Second)      = 0x03  (0b00000011)
+     *  Receive 0x01               = 0x07  (0b00000111)
+     *  During writing group 1     = 0x00  (0b00000000)
+     *  During writing group 2     = 0x10  (0b00010000)
+     *  During writing group 3     = 0x20  (0b00100000)
+     *  During writing group 4     = 0x30  (0b00110000)
+     *  During writing group 5     = 0x40  (0b01000000)
+     *  During writing group 6     = 0x50  (0b01010000)
+     *  During writing group 7     = 0x60  (0b01100000)
+     *  During writing group 8     = 0x70  (0b01110000)
+     *  After Writing              = 0x80  (0b10000000)
+     * ===============================================================================================================
+     */
     UINT index_of_Buffer = 0;
     //FIXME : 
     sendChar(0x77);
     CREN = Bit_High;    //It is needed for integration with OBC
     //TXEN = Bit_High;
+    /* Code
+     * =========================================================================
+     * while(CAM1 == 0){
+     * =========================================================================
+     */
     while((receiveEndH264Flag  & 0x80) != 0x80){
         while (RCIF != 1);
         Buffer[index_of_Buffer] = RCREG;
@@ -184,15 +198,15 @@ void Receive_8split_H264(UDWORD Roop_adr, UDWORD Jump_adr){
             receiveEndH264Flag |= 0x01;
             index_of_Buffer++;
         }
-        else if((receiveEndH264Flag & 0x01) == 0x01 && Buffer[index_of_Buffer] == FooterOfH264[0]){
+        else if((receiveEndH264Flag & 0x01) == 0x01 && Buffer[index_of_Buffer] == FooterOfH264[1]){
             receiveEndH264Flag |= 0x02;
             index_of_Buffer++;
         }
-        else if((receiveEndH264Flag & 0x02) == 0x02 && Buffer[index_of_Buffer] == FooterOfH264[1]){
+        else if((receiveEndH264Flag & 0x02) == 0x02 && Buffer[index_of_Buffer] == FooterOfH264[2]){
             receiveEndH264Flag |= 0x04;
             index_of_Buffer++;
         }
-        else if ((receiveEndH264Flag & 0x04) == 0x04 && Buffer[index_of_Buffer] == FooterOfH264[2])
+        else if ((receiveEndH264Flag & 0x04) == 0x04 && Buffer[index_of_Buffer] == FooterOfH264[3])
         {
             flash_Write_Data(FROM_Write_adr, (UDWORD)(index_of_Buffer + 1), &Buffer);
             index_of_Buffer = 0;
@@ -216,4 +230,9 @@ void Receive_8split_H264(UDWORD Roop_adr, UDWORD Jump_adr){
             index_of_Buffer = 0;
         }
     }
+    /* Code
+     * =========================================================================
+     * }
+     * =========================================================================
+     */
 }

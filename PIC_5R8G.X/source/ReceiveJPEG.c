bcf815accb418c412fad9cc2512417a6fc6a1f84
ryogaide <ide.r.aa@m.titech.ac.jp>
2018-06-06 19:03:05 +0900

    Erase uncorrective using CAM1 & code go into skelton

--- PIC_5R8G.X/source/ReceiveJPEG.c
+++ PIC_5R8G.X/source/ReceiveJPEG.c
@@ -8,16 +8,6 @@
 #include "MAX2828.h"
 #include "typedefine.h"
 
-/* Comment
- * =========================================================================
- * We use 18 sectors by each file.
- * We call two  sectors as one group.
- * Image files use 8 groups and ECC (Error Correction) use 1 groups.
- * Before ECC, we detect EOF(End of File) and jump to next group.
- * After ECC, we receive all data until detecting CAM1-off signal.
- * =========================================================================
- */
-
 void Receive_8split_JPEG(UDWORD Roop_adr, UDWORD Jump_adr){
    /* Comment
     * ==========================================================================
@@ -49,25 +39,10 @@ void Receive_8split_JPEG(UDWORD Roop_adr, UDWORD Jump_adr){
     * ===============================================================================================================
     */
     UINT index_of_Buffer = 0;
-    //FIXME : 
+    //FIXME : debug
     sendChar(0x88);
     CREN = Bit_High;    //It is needed for integration with OBC
     //TXEN = Bit_High;
-    /* Comment
-     * =========================================================================
-     * We no longer use 0x80 flag (Finish of file).
-     * Instead, we will use CAM1 until finishing receiving data.
-     * Thumbnail and H264 is also used CAM1.
-     * CAM1 is bit inversion.
-     * =========================================================================
-     * Code
-     * =========================================================================
-     * while(CAM1 = 1);
-     * while(CAM1 == 0){
-     *      source code
-     * }
-     * =========================================================================
-     */
     while((receiveEndJpegFlag  & 0x80) != 0x80){
         while (RCIF != 1){
             if(CAM1 == 1) break;
@@ -81,17 +56,9 @@ void Receive_8split_JPEG(UDWORD Roop_adr, UDWORD Jump_adr){
         {
             flash_Write_Data(FROM_Write_adr, (UDWORD)(index_of_Buffer + 1), &Buffer);
             index_of_Buffer = 0;
-            
             //  Jump to next group's first sector & change flag
             receiveEndJpegFlag &= ~0x0f;    //Clear low order 4bit of receiveEndJpegFlag. Reset 0xFF flag
             receiveEndJpegFlag += 0x10;     //+1 8split_cnt in receiveEndJpegFlag.
-            /* Code
-             * =================================================================
-             * if(receiveEndJpegFlag < 0x70){    >  //Before ECC
-             *      FROM_Write_adr = Roop_adr +(UINT)(receiveEndJpegFlag >> 4) * Jump_adr;
-             * }
-             * =================================================================
-             */
             FROM_Write_adr = Roop_adr +(UINT)(receiveEndJpegFlag >> 4) * Jump_adr;
             sendChar(receiveEndJpegFlag);
             sendChar((UBYTE)(FROM_Write_adr >> 16));
@@ -127,19 +94,11 @@ void Receive_thumbnail_JPEG(UDWORD Roop_adr, UDWORD Jump_adr){
     *  ----      ----     ----    finish_cnt        ----        ----        Flag_0x0E           Flag_0xFF
     * ====================================================================================================
     */
-    //FIXME : 
+    //FIXME : debug
     sendChar(0xbb);
     CREN = Bit_High;
     TXEN = Bit_High;
     UINT index_of_Buffer = 0;
-    /* Code
-     * =========================================================================
-     * while(CAM1 = 1);
-     * while(CAM1 == 0){
-     *      source code
-     * }
-     * =========================================================================
-     */
     while((receiveEndJpegFlag  & 0x10) != 0x10)
     {
         while (RCIF != 1){
@@ -161,7 +120,6 @@ void Receive_thumbnail_JPEG(UDWORD Roop_adr, UDWORD Jump_adr){
             TXEN = Bit_High;
             sendChar(receiveEndJpegFlag);
             sendChar((UBYTE)(FROM_Write_adr >> 16));
-            
         }
         else
         {
@@ -208,24 +166,17 @@ void Receive_8split_H264(UDWORD Roop_adr, UDWORD Jump_adr){
      * ===============================================================================================================
      */
     UINT index_of_Buffer = 0;
-    //FIXME : 
+    //FIXME : debug
     sendChar(0x77);
     CREN = Bit_High;    //It is needed for integration with OBC
     //TXEN = Bit_High;
     /* Code
      * =========================================================================
-     * while(CAM1 = 1);
-     * while(CAM1 == 0){
-     *      source code
-     * }
-     * =========================================================================
-     */
     while((receiveEndH264Flag  & 0x80) != 0x80){
         while (RCIF != 1){
             if(CAM1 == 1) break;
         }
         Buffer[index_of_Buffer] = RCREG;
-        /* Skelton
         if((receiveEndH264Flag & 0x01) == 0x00 && Buffer[index_of_Buffer] == FooterOfH264[0]){
             receiveEndH264Flag |= 0x01;
             index_of_Buffer++;
@@ -246,11 +197,6 @@ void Receive_8split_H264(UDWORD Roop_adr, UDWORD Jump_adr){
             //  Jump to next group's first sector & change flag
             receiveEndH264Flag &= ~0x0f;    //Clear low order 4bit of receiveEndJpegFlag.
             receiveEndH264Flag += 0x10;     //+1 8split_cnt in receiveEndH264Flag.
-            =================================================================
-            if(receiveEndJpegFlag < 0x70){    >  //Before ECC
-                FROM_Write_adr = Roop_adr +(UINT)(receiveEndJpegFlag >> 4) * Jump_adr;
-            }
-            =================================================================
             FROM_Write_adr = Roop_adr +(UINT)(receiveEndH264Flag >> 4) * Jump_adr;
             sendChar(receiveEndH264Flag);
             sendChar((UBYTE)(FROM_Write_adr >> 16));
@@ -260,13 +206,15 @@ void Receive_8split_H264(UDWORD Roop_adr, UDWORD Jump_adr){
             receiveEndH264Flag &= ~0x0f;    //Clear low order 4bit of receiveEndJpegFlag
             index_of_Buffer++;
         }
-        */
+        
         if(index_of_Buffer == MaxOfMemory){
             flash_Write_Data(FROM_Write_adr, (UDWORD)(MaxOfMemory), &Buffer);
             FROM_Write_adr += (UDWORD)(MaxOfMemory);
             index_of_Buffer = 0;
         }
     }
+     * =========================================================================
+     */
 }
 
 void Receive_ECC(UDWORD Roop_adr, UDWORD Jump_adr, UBYTE *Command_length){
@@ -286,10 +234,16 @@ void Receive_ECC(UDWORD Roop_adr, UDWORD Jump_adr, UBYTE *Command_length){
      * We calc the length of ECC from Command_length
      * =========================================================================
      */
+    /* Code
+     * =========================================================================
     UINT ECC_length = 0;
-    for(UINT i=0; i<3; i++){    
-        ECC_length += *(Command_length + i) << 8*(2-i); 
+    for(UINT i=0; i<3; i++){        >
+        ECC_length += *(Command_length + i) << 8*(2-i); >
     }
+     * =========================================================================
+     */
+    
+    
     
    /*  How to use receiveEndECCFlag
     * ===============================================================================================================
@@ -305,12 +259,14 @@ void Receive_ECC(UDWORD Roop_adr, UDWORD Jump_adr, UBYTE *Command_length){
     sendChar(0xee);
     CREN = Bit_High;    //It is needed for integration with OBC
     //TXEN = Bit_High;
+    /* Code
+     * =========================================================================
     while(receiveEndECCFlag != 0x08){
         while (RCIF != 1){
             if(CAM1 == 1) break;
         }
         Buffer[index_of_Buffer] = RCREG;
-        if (receiveEndECCFlag < 0x07 && ECC_count == ECC_length/8)
+        if (receiveEndECCFlag < 0x07 && ECC_count == ECC_length/8)  >
         {
             flash_Write_Data(FROM_Write_adr, (UDWORD)(index_of_Buffer + 1), &Buffer);
             index_of_Buffer = 0;
@@ -340,4 +296,6 @@ void Receive_ECC(UDWORD Roop_adr, UDWORD Jump_adr, UBYTE *Command_length){
             index_of_Buffer = 0;
         }
     }
+     * =========================================================================
+     */
 }
